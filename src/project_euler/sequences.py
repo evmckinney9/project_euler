2afefc718039ffd3d9fe116e61b827afd577eb4e
Evan McKinney <evmckinney9@gmail.com>
2023-07-16 18:41:14 -0400

    solutions to 44-46

--- src/project_euler/sequences.py
+++ src/project_euler/sequences.py
@@ -1,4 +1,5 @@
 """Project Euler sequences."""
+from functools import lru_cache
 from itertools import permutations
 
 
@@ -12,11 +13,33 @@ def gen_pandigital(k, include_zero=False):
         yield int("".join(str(d) for d in perm))
 
 
+@lru_cache(maxsize=None)
 def triangle_numbers(n):
     """Generate the n-th triangle number."""
     return (n * (n + 1)) // 2
 
 
+@lru_cache(maxsize=None)
 def is_triangle_number(t):
     """Check if a number t is a triangle number."""
     return 0.5 * ((8 * t + 1) ** 0.5 - 1) % 1 == 0
+
+
+@lru_cache(maxsize=None)
+def pentagonal_numbers(n):
+    """Generate the n-th pentagonal number."""
+    return (n * (3 * n - 1)) // 2
+
+
+@lru_cache(maxsize=None)
+def is_pentagonal(t):
+    """Check if a number p is a pentagonal number."""
+    if t < 1:
+        return False
+    return ((24 * t + 1) ** 0.5 + 1) % 6 == 0
+
+
+@lru_cache(maxsize=None)
+def hexagonal_numbers(n):
+    """Generate the n-th hexagonal number."""
+    return n * (2 * n - 1)
