d4a4616b0f8c33489c693673f2faaa0cac8df28b
Evan McKinney <evmckinney9@gmail.com>
2023-07-16 17:36:45 -0400

    p43

--- src/project_euler/sequences.py
+++ src/project_euler/sequences.py
@@ -1,4 +1,15 @@
 """Project Euler sequences."""
+from itertools import permutations
+
+
+def gen_pandigital(k, include_zero=False):
+    """Generate all pandigital numbers of length k, sorted by value."""
+    if include_zero:
+        digits = reversed(list(range(k + 1)))
+    else:
+        digits = reversed(list(range(1, k + 1)))
+    for perm in permutations(digits):
+        yield int("".join(str(d) for d in perm))
 
 
 def triangle_numbers(n):
